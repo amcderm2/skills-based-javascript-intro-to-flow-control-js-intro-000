722b79d728c9d875b8adb1f7639b7c50cae7b9e2
Ashley McDermott <jamcdermott98@gmail.com>
2021-02-21 14:07:38 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,13 +1,16 @@
 function basicTeenager(age) {
-
+  if(13-19)
+  return("You are a teenager!")
 }
 
 function teenager(age) {
-
+  if(13-19)
+  return("You are a teenager!")
 }
 
 function ageChecker(age) {
-
+  if(13-19)
+  return("You are a teenager!")
 }
 
 function ternaryTeenager(age) {
